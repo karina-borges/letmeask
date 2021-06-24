aa3e0cc4f16d008f3b3c337b6ecd5cc6eaf2c9ef
Karina Borges <karina_borges@id.uff.br>
2021-06-23 22:52:07 -0300

    🚧 working in progress day #3

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { AuthContextProvider } from "./contexts/AuthContext";
+import Room from "./pages/Room";
 
 function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <Route path="/" component={Home} exact />
-        <Route path="/rooms/new" component={NewRoom} />
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/rooms/new" component={NewRoom} exact />
+          <Route path="/rooms/:id" component={Room} />
+        </Switch>
       </AuthContextProvider>
     </BrowserRouter>
   );
